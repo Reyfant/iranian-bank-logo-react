ef05bc1f4457fdc96b3056007e7f3004cff0f9c3
CyberHuginn <cyberhuginn@gmail.com>
2025-11-17 00:38:04 +0330

    update

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module "iranian-bank-logo-react" {
     import * as React from "react";
 
     export interface IranianBankProps {
-        name: "ir-blu" | "ir-day" | "ir-eghtesad-novin" | "ir-gardeshgari" | "ir-iranzamin" | "ir-karafarin" | "ir-keshavarzi" | "ir-markazi" | "ir-maskan" | "ir-mehr" | "ir-mellat" | "ir-melli" | "ir-parsian" | "ir-pasargad" | "ir-post-bank" | "ir-refah" | "ir-resalat" | "ir-saderat" | "ir-saman" | "ir-sanat-madan" | "ir-sepah" | "ir-shahr" | "ir-sina" | "ir-tejarat" | "ir-tose-taawon";
+        name: "ir-blu" | "ir-day" | "ir-eghtesad-novin" | "ir-gardeshgari" | "ir-iranzamin" | "ir-karafarin" | "ir-keshavarzi" | "ir-markazi" | "ir-maskan" | "ir-mehr" | "ir-mellat" | "ir-melli" | "ir-parsian" | "ir-pasargad" | "ir-post-bank" | "ir-refah" | "ir-resalat" | "ir-saderat" | "ir-saman" | "ir-sanat-madan" | "ir-sepah" | "ir-shahr" | "ir-sina" | "ir-tejarat" | "ir-tose-taawon" | "unknown" | string | null;
         size?: number;
         [key: string]: any;
     }
