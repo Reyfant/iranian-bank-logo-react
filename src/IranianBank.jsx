ec67b3eacc2d3b44089e0db4a088a4e37da449e7
CyberHuginn <cyberhuginn@gmail.com>
2025-11-16 11:09:16 +0330

    add iconMap test

--- src/IranianBank.jsx
+++ src/IranianBank.jsx
@@ -1,24 +1,25 @@
 'use client'
 import React from "react";
+import { iconMap } from "./iconMap";
 
 const IranianBank = ({ name, size = 20, ...rest }) => {
-    try {
-        const Icon = require(`./icons/${name}.svg`).default;
+    const Icon = iconMap[name];
 
-        return (
-            <img
-                src={Icon}
-                width={size}
-                height={size}
-                style={{ width: `${size}px`, height: `${size}px` }}
-                alt={name}
-                {...rest}
-            />
-        );
-    } catch (e) {
+    if (!Icon) {
         console.error(`Icon "${name}" not found.`);
         return null;
     }
+
+    return (
+        <img
+            src={Icon}
+            width={size}
+            height={size}
+            style={{ width: `${size}px`, height: `${size}px` }}
+            alt={name}
+            {...rest}
+        />
+    );
 };
 
 export default IranianBank;
---
+++ src/iconMap.js
@@ -0,0 +1,6 @@
+export const iconMap = {
+    mellat: require("./icons/ir-mellat.svg"),
+    pasargad: require("./icons/ir-pasargad.svg"),
+    tejarat: require("./icons/ir-tejarat.svg"),
+    day: require("./icons/ir-day.svg"),
+};
